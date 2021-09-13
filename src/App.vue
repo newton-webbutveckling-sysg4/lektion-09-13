bb19b027d592bf09eebca3a3922fde3916c10a79
David Andersson <david.andersson@zocom.se>
2021-09-13 11:32:29 +0200

    Simple AJAX component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-
+	<SimpleAjax></SimpleAjax>
 	<HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import SimpleAjax from './components/SimpleAjax.vue'
 export default {
 	name: 'App',
 	components: {
-		HelloWorld
+		HelloWorld, SimpleAjax
 	}
 }
 </script>
---
+++ src/components/SimpleAjax.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="component">
+	<h1> Simple AJAX </h1>
+	<button v-on:click="sendRequest"> Send request </button>
+	<p> {{message}} </p>
+</div>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		message: 'No response yet'
+	}),
+	methods: {
+		async sendRequest() {
+			const url = 'https://forverkliga.se/JavaScript/api/simple.php?key=value'
+			try {
+				// Fetch skickar ett GET request till URL
+				const response = await fetch(url)
+				const data = await response.json()
+				console.log('Data from API:', data);
+				this.message = `Message: ${data.message}. Time: ${data.time}`
+			}
+			catch(error) {
+				this.message = 'Something went wrong. Please try again later.'
+			}
+		}  // sendRequest
+	}  // methods
+}
+</script>
+
+<style scoped>
+.component {
+	border: 1px solid gray;
+	padding: 1em;
+	margin: 1em;
+	background: #ACD7F9;
+}
+</style>
